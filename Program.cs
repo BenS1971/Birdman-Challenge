c38f3a966e013b15680b55404e15a2e161aa5a69
BenS1971 <140563198+BenS1971@users.noreply.github.com>
2023-09-13 20:52:41 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+Console.WriteLine("Drofsnar The Birdman cometh!");
+Console.WriteLine("Drofsnar starts with 5000 points and 3 lives...");
+Console.WriteLine("5000 points 3 Lives");
+Console.WriteLine("Drofsnar encounters 11 birds.");
+Console.WriteLine("Drofsnar is attacked by Invincible Bird Hunter! He loses one life, 2 lives remain.");
+
+int additionResult = 5000 + 110;
+Console.WriteLine(additionResult+" points with 2 lives remaining.");
+
+namespace Classes
+{
+class Bird
+  {
+    string BirdType = "Grosbeam";
+    string BirdType = "Prarie Chicken";
+    string BirdType = "Iceland Gull";
+    string BirdType = "Crested Ibis";
+
+    string BirdType = "Great KisKudee";
+    String BirdType = "Orange-Bellied Parrot";
+    }
+}
+
